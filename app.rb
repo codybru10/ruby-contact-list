f2aba3c97c845f32288df5bd0fa1387c4d42a309
Cody Brubaker and Joshua Rinard <joshua@>
2016-12-01 11:25:58 -0800

    add submit contact form and viewing list

--- app.rb
+++ app.rb
@@ -11,3 +11,17 @@ get('/contacts') do
   @contacts = Contact.all()
   erb(:contact_list)
 end
+
+get('/contacts/new') do
+  erb(:contact_form)
+end
+
+post('/contacts') do
+  @first_name = params.fetch("first_name")
+  @last_name = params.fetch("last_name")
+  @job_title = params.fetch("job_title")
+  @company = params.fetch("company")
+  Contact.new({:first_name => @first_name, :last_name => @last_name, :job_title => @job_title, :company => @company}).save()
+  @contacts = Contact.all()
+  erb(:success)
+end
--- spec/contacts_integration_spec.rb
+++ spec/contacts_integration_spec.rb
@@ -7,6 +7,23 @@ describe('the contact list file path', {:type => :feature}) do
   it('processes the users click and return the contact list page') do
     visit('/')
     click_link('See Contact List')
-    expect(page).to have_content('Contacts')
+    expect(page).to have_content('Contact List')
+  end
+end
+
+describe('the add contact list file path', {:type => :feature}) do
+  it('processes the users click and return the add contact form page') do
+    visit('/')
+    click_link('Add Contact')
+    expect(page).to have_content('Add Contact:')
+  end
+end
+
+describe('add new contact to list and success file path', {:type => :feature}) do
+  it('processes users input and click to return success page') do
+    visit('/contacts/new')
+    fill_in('first_name', :with => 'Bruce')
+    click_button('Save')
+    expect(page).to have_content('Contact Saved')
   end
 end
