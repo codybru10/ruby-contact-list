e58e19ee4b8aad8f4389ad6c642760db02a3cc99
Cody Brubaker and Joshua Rinard <cody@>
2016-12-01 09:36:00 -0800

    adds passing specs for first name, last name, job title, and company

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,9 +1,12 @@
 class Contact
-  attr_reader(:first_name)
+  attr_reader(:first_name, :last_name, :job_title, :company)
 
   @@contacts = []
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @job_title = attributes.fetch(:job_title)
+    @company = attributes.fetch(:company)
   end
 
 
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -5,8 +5,29 @@ describe(Contact) do
 
   describe('#first_name') do
     it('returns the name of the contact') do
-      test_contact = Contact.new({:first_name =>'Bruce'})
+      test_contact = Contact.new({:first_name =>'Bruce', :last_name => 'Wayne', :job_title => "CEO", :company => "Wayne Enterprises"})
       expect(test_contact.first_name()).to(eq("Bruce"))
+    end
+  end
+
+  describe('#last_name') do
+    it('returns the name of the contact') do
+      test_contact = Contact.new({:first_name =>'Bruce', :last_name => 'Wayne', :job_title => "CEO", :company => "Wayne Enterprises"})
+      expect(test_contact.last_name()).to(eq("Wayne"))
+    end
+  end
+
+  describe('#job_title') do
+    it('returns the job title') do
+      test_contact = Contact.new({:first_name =>'Bruce', :last_name => 'Wayne', :job_title => "CEO", :company => "Wayne Enterprises"})
+      expect(test_contact.job_title()).to(eq("CEO"))
+    end
+  end
+
+  describe('#company') do
+    it('returns the name of the contact') do
+      test_contact = Contact.new({:first_name =>'Bruce', :last_name => 'Wayne', :job_title => "CEO", :company => "Wayne Enterprises"})
+      expect(test_contact.company()).to(eq("Wayne Enterprises"))
     end
   end
 
